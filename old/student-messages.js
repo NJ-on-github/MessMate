3eaf61c080d944187b9f365cb8ee8f55396855cc
Nishit Jain <nishit12345.jain@gmail.com>
2025-06-28 02:03:19 +0530

    Removed older versions, improved organization

--- old/student-messages.js
+++
@@ -1,8 +0,0 @@
-let notApproved_main = "Your Account Has Not Been Approved Yet";
-let notApproved_sub = "Please wait for some time or contact your mess manager";
-
-let notFound_main = "Your Account Has Not Been Found";
-let notFound_sub = "Please check your credentials and try again";
-
-let acc_blocked = "Your Account Has Been Blocked";
-
